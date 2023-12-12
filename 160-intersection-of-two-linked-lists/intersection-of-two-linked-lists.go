6e3e31493b718fa14e906ccc97813c2423f61206
manistiwari31 <146080741+manistiwari31@users.noreply.github.com>
2023-12-12 08:57:54 +0530

    Time: 22 ms (98.43%) | Memory: 6.9 MB (59.58%) - LeetSync

---
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.go
@@ -0,0 +1,13 @@
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+    seen := make(map[*ListNode]bool)
+    for n := headA; n != nil; n = n.Next {
+        seen[n] = true
+    }
+
+    for n := headB; n != nil; n = n.Next {
+        if seen[n] {
+            return n
+        }
+    }
+    return nil
+}
