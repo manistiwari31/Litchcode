623b45cdbe54a982f5010bb17efae42c201b9cf9
manistiwari31 <146080741+manistiwari31@users.noreply.github.com>
2023-12-31 10:29:09 +0530

    Time: 61 ms (16.10%) | Memory: 17.3 MB (10.73%) - LeetSync

---
+++ Litchcode/dp/1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+        li = []
+        for i in range(len(s)):
+            for j in range(i+1,len(s)):
+                if s[i]==s[j]:
+                    li.append(j-i-1)
+        if len(li)==0:
+            return -1
+        else:
+            return max(li)
+
+        
