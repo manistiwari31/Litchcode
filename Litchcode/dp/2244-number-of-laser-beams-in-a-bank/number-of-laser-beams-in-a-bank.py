7887e68b1472cdc712f17f96f00c8b2f8f22bd85
manistiwari31 <146080741+manistiwari31@users.noreply.github.com>
2024-01-03 22:14:39 +0530

    Time: 156 ms (36.73%) | Memory: 19.4 MB (12.99%) - LeetSync

---
+++ Litchcode/dp/2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        if len(bank) < 2:
+            return 0
+
+        solution = 0
+        beacons_in_prev_row = 0
+        beacons_in_current_row = 0
+        for row in bank:
+            beacons_in_current_row = 0
+            for c in row:
+                if c == '1':
+                    beacons_in_current_row += 1
+            solution += beacons_in_current_row * beacons_in_prev_row
+            beacons_in_prev_row = beacons_in_current_row or beacons_in_prev_row
+
+        return solution
