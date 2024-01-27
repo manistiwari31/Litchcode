2e80d10cb16484c96eb497b8de1ac3b1bed45e17
manistiwari31 <146080741+manistiwari31@users.noreply.github.com>
2024-01-27 07:41:35 +0530

    Time: 115 ms (96.43%) | Memory: 18 MB (63.33%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
@@ -1,8 +1,32 @@
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
-        cmin, cmax = 0, 0
-        for i, j in zip(s, locked):
-            cmin -= 1 if cmin and (j == '0' or i == ')') else -1
-            cmax += 1 if j == '0' or i == '(' else -1
-            if cmax < 0: return False
-        return cmin == 0
+        n = len(locked)
+        if n%2 is 1: # Obviously this results in an imbalance of parantheses
+            return False
+        unlockedSlots = 0
+        unlockedLefts = 0 #Unlocked slots currently used to match lefts
+        unmatchedLefts = 0 #Lefts that haven't found a match
+        for i in range(n):
+            if locked[i] is '0':
+                if unmatchedLefts > 0:
+                    unlockedLefts += 1
+                    unmatchedLefts -= 1
+                unlockedSlots += 1
+            else: # locked[i] is 1
+                if s[i] is '(':
+                    unmatchedLefts += 1
+                elif s[i] is ')':
+                    if unmatchedLefts is 0 and unlockedSlots is 0:
+                        return False
+                    elif unmatchedLefts > 0:
+                        unmatchedLefts -= 1
+                    elif unlockedSlots > unlockedLefts: 
+                        unlockedSlots -= 1
+                    else:
+                        unmatchedLefts += 1
+                        unlockedSlots -= 1
+        if unmatchedLefts > 0:
+            return False
+        return True
+
+            
