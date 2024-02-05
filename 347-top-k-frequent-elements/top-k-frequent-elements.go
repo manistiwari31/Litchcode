5f1faae70d21ae460980f08db3e51066a8392154
manistiwari31 <146080741+manistiwari31@users.noreply.github.com>
2024-02-05 08:25:44 +0530

    Time: 13 ms (54.74%) | Memory: 6 MB (83.30%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.go
+++ 347-top-k-frequent-elements/top-k-frequent-elements.go
@@ -1,20 +1,27 @@
+
 func topKFrequent(nums []int, k int) []int {
-	f := map[int]int{} // freq of each element
-    maxFreq := 0
-	for i := range nums {
-		f[nums[i]]++
-        maxFreq = max(maxFreq,f[nums[i]])
-	}    
+    if k == 0 {
+        return []int{}
+    }
+    countMap := make(map[int]int)
 
-    buckets := make([][]int,maxFreq+1)
-    for key,freq := range f {
-        buckets[freq] = append(buckets[freq],key)
+    for _,n := range nums {
+        countMap[n] += 1
     }
 
-    nums = make([]int,0)
-    for i := maxFreq; i >= 0 && k > 0  ; i-- {
-        k-= len(buckets[i]) // answer is unique; so it cannot be that we take 2 elements from same freq and leave third
-        nums = append(nums,buckets[i]...)
+    frequencyMap := make(map[int][]int)
+    for key,v := range countMap {
+        frequencyMap[v] = append(frequencyMap[v], key)
     }
-    return nums
+
+    result := []int{}
+    for i := len(nums); i > 0; i-- {
+        if len(result) >= k {
+            break
+        }
+
+        result = append(result, frequencyMap[i]...)       
+    }
+
+    return result
 }
