291a3a0363c4bd7491ac311b1a205ffb8d14e4e4
manistiwari31 <146080741+manistiwari31@users.noreply.github.com>
2024-02-05 08:27:04 +0530

    Time: 14 ms (41.06%) | Memory: 6 MB (83.30%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.go
+++ 347-top-k-frequent-elements/top-k-frequent-elements.go
@@ -1,27 +1,37 @@
-
 func topKFrequent(nums []int, k int) []int {
-    if k == 0 {
-        return []int{}
+    m := make(map[int]int)
+    for _, val := range nums {
+        m[val] = m[val]+1
     }
-    countMap := make(map[int]int)
-
-    for _,n := range nums {
-        countMap[n] += 1
-    }
-
-    frequencyMap := make(map[int][]int)
-    for key,v := range countMap {
-        frequencyMap[v] = append(frequencyMap[v], key)
-    }
-
-    result := []int{}
-    for i := len(nums); i > 0; i-- {
-        if len(result) >= k {
-            break
+    pq := make([]Item, k)
+    i := 0
+    for key, val := range m {
+        if i<len(pq) {
+            pq[i] = Item{value: key, count: val}
+            i++
+        } else {
+            min := math.MaxInt32
+            minInd := -1
+            for j, v := range pq {
+                if v.count < min {
+                    min = v.count
+                    minInd = j
+                }
+            }
+            if val > min {
+                pq[minInd] = Item{value: key, count: val}
+            }
         }
-
-        result = append(result, frequencyMap[i]...)       
     }
+    sort.Slice(pq, func(i, j int) bool { return pq[i].count > pq[j].count })
+    res := make([]int, k)
+    for j, v := range pq {
+        res[j] = v.value
+    }            
+    return res
+}
 
-    return result
+type Item struct {
+	value int 
+	count int   
 }
