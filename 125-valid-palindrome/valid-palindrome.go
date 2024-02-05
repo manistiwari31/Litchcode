70116adc393b820b1def270c5b78562941fa49cb
manistiwari31 <146080741+manistiwari31@users.noreply.github.com>
2024-02-06 05:06:11 +0530

    Time: 5 ms (50.31%) | Memory: 2.7 MB (92.17%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.go
@@ -0,0 +1,28 @@
+func isPalindrome(s string) bool {
+	start, end := 0, len(s)-1
+
+	for start < end {
+		for start < end && !isAlphaNumeric(int(s[start])) {
+			start++
+		}
+
+		for start < end && !isAlphaNumeric(int(s[end])) {
+			end--
+		}
+
+		if strings.ToLower(string(s[start])) != strings.ToLower(string(s[end])) {
+			return false
+		}
+
+		start++
+		end--
+	}
+
+	return true
+}
+
+func isAlphaNumeric(asciCode int) bool {
+	return (int('a') <= asciCode && asciCode <= int('z')) ||
+		(int('A') <= asciCode && asciCode <= int('Z')) ||
+		(int('0') <= asciCode && asciCode <= int('9'))
+}
