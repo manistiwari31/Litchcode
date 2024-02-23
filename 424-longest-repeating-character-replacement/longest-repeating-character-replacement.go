5bbf9aba45137bb030ed26f899d831970fc6a200
manistiwari31 <146080741+manistiwari31@users.noreply.github.com>
2024-02-23 05:36:57 +0530

    Time: 7 ms (78.45%) | Memory: 2.5 MB (25.70%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.go
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.go
@@ -1,19 +1,28 @@
 func characterReplacement(s string, k int) int {
+	// We can also use a  count := make([]int, 91) and it will do exact same Job ("Z"'s ASCII value is 90)
     count := make(map[byte]int)
+    res, maxf, left := 0, 0, 0
+	
+    for right := 0; right < len(s); right++ {
+        count[s[right]] += 1    // s[right] will return a byte. Incase we used slice instead of hashmap this byte is the same as that element's position in the count slice
 
-	l := 0
-	maxf := 0
-	for r := 0; r < len(s); r++ {
-		count[s[r]] = 1 + count[s[r]]
-		if count[s[r]] > maxf {
-			maxf = count[s[r]]
-		}
+        maxf = max(maxf, count[s[right]])
+        
+        // Move left pointer
+        if (right - left + 1) - maxf > k  {
+            count[s[left]] -= 1  // When we decrement, we dont have to update max variable as reducing max variable wont have any effect on end result
+            left += 1
+        }
 
-		if (r - l + 1) - maxf > k {
-			count[s[l]] -= 1
-			l++
-		}
-	}
+        res = max(res, right - left + 1)
+    }
 
-	return len(s) - l
+    return res
+}
+
+func max(a,b int) int {
+    if a > b {
+        return a
+    }
+    return b
 }
