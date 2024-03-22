f37a2834c9691f46d9021bf5bec4082920a2fffe
manistiwari31 <146080741+manistiwari31@users.noreply.github.com>
2024-03-22 05:44:24 +0530

    Time: 1 ms (76.47%) | Memory: 2.3 MB (8.82%) - LeetSync

--- 75-sort-colors/sort-colors.go
+++ 75-sort-colors/sort-colors.go
@@ -1,9 +1,25 @@
+
 func sortColors(nums []int) {
-    for i := 0; i < len(nums)-1; i++ {
-        for j := i; j < len(nums); j++ {
-            if nums[i] > nums[j] {
-                nums[i], nums[j] = nums[j], nums[i]
-            }
+    quickSort(nums, 0, len(nums)-1)
+}
+
+func quickSort(nums []int, low, high int) {
+    if low < high {
+        pivotIndex := partition(nums, low, high)
+        quickSort(nums, low, pivotIndex-1)
+        quickSort(nums, pivotIndex+1, high)
+    }
+}
+
+func partition(nums []int, low, high int) int {
+    pivot := nums[high]
+    i := low - 1
+    for j := low; j < high; j++ {
+        if nums[j] < pivot {
+            i++
+            nums[i], nums[j] = nums[j], nums[i]
         }
     }
+    nums[i+1], nums[high] = nums[high], nums[i+1]
+    return i + 1
 }
